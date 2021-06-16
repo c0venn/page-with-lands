b159b60a129605fd1d85db4ab64cdc0edf288ae5
c0venn <fabichidori2@gmail.com>
2021-06-16 20:08:31 +0000

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./component/navbar";
 import Custom404 from "./views/404";
+import Hoome from "./views/Home";
 import House from "./views/House";
 import About from "./views/about";
 
@@ -11,6 +12,7 @@ export function Home() {
 			<BrowserRouter>
 				<Navbar />
 				<Switch>
+					<Route exact path="/" component={Hoome} />
 					<Route exact path="/house" component={House} />
 					<Route exact path="/about" component={About} />
 					<Route component={Custom404} />
